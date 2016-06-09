a81176cb5a303a1ad16022f98844fc37bb257f4b
Jonathan Chauncey <jchauncey@deis.com>
2016-06-09 16:12:18 -0600

    chore(logs): Rename previous logs to pod-previous.log This way you aren't forced to download them to open them.

--- scripts/deis.sh
+++ scripts/deis.sh
@@ -82,6 +82,6 @@ get-pod-logs() {
   pods=$(kubectl get pods --namespace=deis | sed '1d' | awk '{print $1}')
   while read -r pod; do
     kubectl logs "${pod}" --namespace=deis >> "${DEIS_LOG_DIR}/${pod}.log"
-    kubectl logs "${pod}" -p --namespace=deis >> "${DEIS_LOG_DIR}/${pod}.previous"
+    kubectl logs "${pod}" -p --namespace=deis >> "${DEIS_LOG_DIR}/${pod}-previous.log"
   done <<< "$pods"
 }
