1909053b812ae49bd05eacaee3ad80fb203cc9df
Keerthan Reddy Mala (kmala) <keerthan.mala@gmail.com>
2016-10-13 13:24:03 -0600

    feat(helm): add support for workflow testing using the new helm

--- scripts/run.sh
+++ scripts/run.sh
@@ -17,25 +17,35 @@ lease
 # Get node information
 kubectl get nodes
 
-# Get Helm up to date and checkout branch if needed
-echo "Adding repo ${HELM_REMOTE_REPO}"
-helmc repo add deis "${HELM_REMOTE_REPO}"
-echo "Get helm up to date"
-helmc up
-cd "${DEIS_CHART_HOME}" || exit
-echo "Checking out ${WORKFLOW_BRANCH} for ${WORKFLOW_CHART}"
-git checkout "${WORKFLOW_BRANCH}"
-helmc fetch "deis/${WORKFLOW_CHART}"
-
 # Clean cluster if needed
 echo "Cleaning cluster if needed"
 clean_cluster
 
-# Install $WORKFLOW_CHART
-echo "Generate manifests from templates"
-helmc generate "${WORKFLOW_CHART}"
-echo "Installing chart ${WORKFLOW_CHART}"
-helmc install "${WORKFLOW_CHART}" &> /dev/null
+if [ -n "$USE_KUBERNETES_HELM" ]
+then
+  echo "Installing kubernetes helm"
+  install_helm
+  echo "Adding  deis workflow chart repo"
+  helm repo add workflow-dev https://charts.deis.com/workflow-dev
+  echo "Installing chart workflow-${RELEASE_TAG}"
+  helm install workflow-dev/workflow --version="${RELEASE_TAG}" --namespace=deis
+else
+  # Get Helm up to date and checkout branch if needed
+  echo "Adding repo ${HELM_REMOTE_REPO}"
+  helmc repo add deis "${HELM_REMOTE_REPO}"
+  echo "Get helm up to date"
+  helmc up
+  cd "${DEIS_CHART_HOME}" || exit
+  echo "Checking out ${WORKFLOW_BRANCH} for ${WORKFLOW_CHART}"
+  git checkout "${WORKFLOW_BRANCH}"
+  helmc fetch "deis/${WORKFLOW_CHART}"
+
+  # Install $WORKFLOW_CHART
+  echo "Generate manifests from templates"
+  helmc generate "${WORKFLOW_CHART}"
+  echo "Installing chart ${WORKFLOW_CHART}"
+  helmc install "${WORKFLOW_CHART}" &> /dev/null
+fi
 
 # Dump log data to stdout
 echo "Running kubectl describe pods and piping the output to ${DEIS_DESCRIBE}"
@@ -48,14 +58,23 @@ deis_healthcheck
 echo "Use http://grafana.$(get-router-ip).nip.io/ to monitor the e2e run"
 
 # Install e2e chart
-echo "Installing workflow-e2e chart ${WORKFLOW_E2E_CHART}"
-cd "${DEIS_CHART_HOME}" || exit
-echo "Checking out ${WORKFLOW_E2E_BRANCH} for ${WORKFLOW_E2E_CHART}"
-git checkout "${WORKFLOW_E2E_BRANCH}"
-helmc fetch "deis/${WORKFLOW_E2E_CHART}"
-helmc generate "${WORKFLOW_E2E_CHART}"
-echo "Installing ${WORKFLOW_E2E_CHART}"
-helmc install "${WORKFLOW_E2E_CHART}" &> /dev/null
+if [ -n "$USE_KUBERNETES_HELM" ]
+then
+  echo "Adding deis e2e chart repo"
+  helm repo add workflow-e2e https://charts.deis.com/workflow-e2e
+  echo "Installing workflow-e2e chart workflow-e2e-${WORKFLOW_E2E_TAG}"
+  helm install workflow-e2e/workflow-e2e --version="${WORKFLOW_E2E_TAG}" --namespace=deis
+  WORKFLOW_E2E_CHART=workflow-${WORKFLOW_E2E_TAG}-e2e
+else
+  echo "Installing workflow-e2e chart ${WORKFLOW_E2E_CHART}"
+  cd "${DEIS_CHART_HOME}" || exit
+  echo "Checking out ${WORKFLOW_E2E_BRANCH} for ${WORKFLOW_E2E_CHART}"
+  git checkout "${WORKFLOW_E2E_BRANCH}"
+  helmc fetch "deis/${WORKFLOW_E2E_CHART}"
+  helmc generate "${WORKFLOW_E2E_CHART}"
+  echo "Installing ${WORKFLOW_E2E_CHART}"
+  helmc install "${WORKFLOW_E2E_CHART}" &> /dev/null
+fi
 
 echo "Running kubectl describe pod ${WORKFLOW_E2E_CHART} and piping the output to ${DEIS_DESCRIBE}"
 kubectl describe pod "${WORKFLOW_E2E_CHART}" --namespace=deis >> "${DEIS_DESCRIBE}" 2> /dev/null
