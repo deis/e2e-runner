cd141830ee8e11851a49f65b4fa1d739dc32843d
Vaughn Dice <vaughn@deis.com>
2017-01-03 13:22:30 -0700

    fix(scripts/deis.sh): check deis namespace

--- tests/stubs/tpl/kubectl.bash
+++ tests/stubs/tpl/kubectl.bash
@@ -1,10 +1,8 @@
 generate() {
-  PODS_OUTPUT=${1}
-  NS_OUTPUT=${2}
+  NS_OUTPUT=${1}
 
   cat <<EOF
     case "\${2}" in
-      ("pods") echo '"${PODS_OUTPUT:-deis-controller}"' ;;
       ("ns") echo '"${NS_OUTPUT:-deis}"' ;;
     esac
 EOF
